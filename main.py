16a14d7280224ca7652dac65a4bf63a06ab58262
jai <jailene@Jailenes-MBP.home>
2021-07-22 20:02:18 -0400

    added to main

--- main.py
+++ main.py
@@ -11,15 +11,16 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
 db = SQLAlchemy(app)
 
 class VideoModel(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(100), nullable=False)
-    views = db.Column(db.Integer, nullable=False)
-    likes = db.Column(db.Integer, nullable=False)
+	id = db.Column(db.Integer, primary_key=True)
+	name = db.Column(db.String(100), nullable=False)
+	views = db.Column(db.Integer, nullable=False)
+	likes = db.Column(db.Integer, nullable=False)
 
-    def __repr__(self):
+    # def __repr__(self):
+    #     pass
         # return "name and stuff"
-        return f"Video(name ={name}, views={views}, likes ={likes}"
-        
+        # return f"Video(name ={name}, views={views}, likes ={likes}"
+   
 video_put_args = reqparse.RequestParser()
 video_put_args.add_argument("name", type=str, help="Name of the video is required", required=True)
 video_put_args.add_argument("views", type=int, help="Views of the video", required=True)
@@ -37,12 +38,14 @@ class Video(Resource):
     def get(self, video_id):
         result = VideoModel.query.get(id= video_id)
         return result
-
-
+        
+    @marshal_with(resource_fields)
     def put(self, video_id):
-        args
-        video = VideoModel(id=video_id, name=)
-
+        args = video_put_args.parse_args()
+	    result = VideoModel.query.filter_by(id= video_id).first()
+	    if result:
+		    abort(409, message="Video id taken...")
+    
     def delete(self, video_id):
         abort_if_video_id_doesnt_exist(video_id)
         del videos[video_id]
